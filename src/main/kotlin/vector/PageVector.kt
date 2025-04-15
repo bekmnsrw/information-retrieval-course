6889106639583a361475e6ed1a3822e3ef656455
bekmnsrw <ilya.bekmansurov@gmail.com>
2025-04-16 00:33:40 +0300

    Added vector search

---
+++ src/main/kotlin/vector/PageVector.kt
@@ -0,0 +1,9 @@
+package vector
+
+/**
+ * @author i.bekmansurov
+ */
+internal class PageVector(
+    val tfIdf: Map<String, Double>,
+    val norm: Double,
+)
---
+++ src/main/kotlin/vector/SearchResult.kt
@@ -0,0 +1,9 @@
+package vector
+
+/**
+ * @author i.bekmansurov
+ */
+internal class SearchResult(
+    val url: String,
+    val cosineSimilarity: Double,
+)
